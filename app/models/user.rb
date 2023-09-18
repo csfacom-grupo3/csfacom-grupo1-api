f4961a28da0bce189623e0cfd3f374fef9970426
Samuel Willian <samuel-1998@hotmail.com.br>
2023-09-18 19:04:50 -0400

    Adicionado permissão ao usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  extend Enumerize
   include TokenAuthenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  serialize :permission, Array
+  enumerize :permission, in: [:projects, :news, :admin], multiple: true, predicates: true
 
 end
