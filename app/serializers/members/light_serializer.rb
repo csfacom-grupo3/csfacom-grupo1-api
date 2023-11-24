76747fbffc9cceedc47a397c4685bd3ede94e398
Samuel Willian <samuel-1998@hotmail.com.br>
2023-11-24 11:20:45 -0400

    resolvido erro de project id

--- app/serializers/members/light_serializer.rb
+++ app/serializers/members/light_serializer.rb
@@ -1,5 +1,5 @@
 class Members::LightSerializer < ActiveModel::Serializer
-  attributes :id, :avatar_url, :github, :linkedin, :email, :name, :description, :project_id
+  attributes :id, :avatar_url, :github, :linkedin, :email, :name, :description
 
   belongs_to :academic_bond, serializer: AcademicBond::LightSerializer
 
